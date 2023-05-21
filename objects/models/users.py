a48fbe1697bc70bab3769bec09f3787bec554583
Sbtah <shisastore@protonmail.com>
2023-05-21 21:49:29 +0200

    Update User model.

--- objects/models/users.py
+++ objects/models/users.py
@@ -27,8 +27,9 @@ class UserManager(BaseUserManager):
         user = self.create_user(
             email=email,
             full_name=full_name,
+            password=password
         )
-        user.is_admin = True
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
@@ -45,7 +46,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     full_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
     objects = UserManager()
