e8e1e3e60545b804080e6fa82c36c8451efe3ed7
Sbtah <shisastore@protonmail.com>
2023-05-21 18:59:19 +0200

    Added User with UserManager.

--- objects/models/users.py
+++ objects/models/users.py
@@ -1,17 +1,38 @@
-from django.db import models
 from django.contrib.auth.models import (
-    BaseUserManager,
     AbstractBaseUser,
+    BaseUserManager,
     PermissionsMixin,
 )
+from django.db import models
+from django.utils import timezone
 
 
 class UserManager(BaseUserManager):
     """Custom UserManager model."""
 
-    def create_user(self, email, full_name, password=None, **extra_field):
+    def create_user(self, email, full_name, password=None):
+
         if not email:
-            pass
+            raise ValueError('Users must have an email address.')
+        user = self.model(
+                email=self.normalize_email(email),
+                full_name=full_name,
+            )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, full_name, password=None):
+
+        user = self.create_user(
+            email=email,
+            full_name=full_name,
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -22,9 +43,12 @@ class User(AbstractBaseUser, PermissionsMixin):
         unique=True,
         verbose_name='email address',
     )
-    full_name = models.CharField(max_length=True)
+    full_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['full_name']
