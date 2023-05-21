a81e94fdb03d085390412009efa4f18342c79ac6
Sbtah <shisastore@protonmail.com>
2023-05-21 22:50:19 +0200

    Added example_local_store fixture.

--- conftest.py
+++ conftest.py
@@ -13,3 +13,15 @@ def example_ecommerce_store():
         domain='test-store.com',
         discovery_url='https://test-store.com/',
     )
+
+
+@pytest.fixture
+def example_local_store(example_ecommerce_store):
+    '''Pytest fixture for creating example LocalSore object.'''
+    e_store = example_ecommerce_store
+    return stores.LocalStore.objects.create(
+        parrent_store=e_store,
+        name='New York',
+        scraped_id=1,
+        is_active=True,
+    )
