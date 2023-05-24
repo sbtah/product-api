9a38079eddc65e1be07b36a528127df70d3c9b81
Sbtah <shisastore@protonmail.com>
2023-05-24 17:23:07 +0200

    Made some changes in test cases for EcommerceStore object. Renamed module.

--- tests/test_ecomerce_store_model.py
+++ tests/test_ecomerce_store_model.py
@@ -2,7 +2,9 @@
 Tests for EcommerceStore model.
 '''
 import pytest
-from objects.models import stores
+
+from scraped.models import ScrapedObject
+from scraped.models import EcommerceStore
 
 
 pytestmark = pytest.mark.django_db
@@ -12,19 +14,19 @@ class TestEcommerceStoreModel:
     '''Test cases related to EcommerceStore objects.'''
 
     def test_create_ecommerce_store(self):
-        '''Test creating EcommerceStore object is sucessful.'''
+        '''Test creating EcommerceStore object is successful.'''
 
-        assert stores.EcommerceStore.objects.all().count() == 0
-        e_store = stores.EcommerceStore.objects.create(
-            name="Test Store",
-            domain="test-store.com",
+        assert EcommerceStore.objects.all().count() == 0
+        e_store = EcommerceStore.objects.create(
+            name='Test Store',
+            domain='test-store.com',
             discovery_url='https://test-store.com/',
         )
-        assert stores.EcommerceStore.objects.all().count() == 1
-        assert isinstance(e_store, stores.EcommerceStore) is True
+        assert EcommerceStore.objects.all().count() == 1
+        assert isinstance(e_store, EcommerceStore) is True
 
     def test_ecommerce_store_str_method(self, example_ecommerce_store):
-        '''Test that __str__ for EcommerceStore is properly generated.'''
+        '''Test that __str__ for EcommerceStore is generating proper output.'''
 
         e_store = example_ecommerce_store
         assert str(e_store) == e_store.name
@@ -36,4 +38,4 @@ class TestEcommerceStoreModel:
 
         assert e_store.created is not None
         assert isinstance(e_store.created, int)
-        assert issubclass(stores.EcommerceStore, stores.ScrapedObject)
+        assert issubclass(EcommerceStore, ScrapedObject)
