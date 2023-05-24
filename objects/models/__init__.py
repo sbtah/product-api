1830fa4ca7383d53cbeb1dbdd198ba2d30b1375b
Sbtah <shisastore@protonmail.com>
2023-05-24 17:25:30 +0200

    Redesigned project structure.

--- objects/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from objects.models.users import User
---
+++ tests/test_product_model.py
@@ -0,0 +1,8 @@
+'''
+Tests for Product model.
+'''
+import pytest
+from scraped.models import Product
+from scraped.models import ScrapedObject
+
+
