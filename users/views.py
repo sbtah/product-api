822c168722fad0b562abf2f14b60e69fbce0495c
Sbtah <shisastore@protonmail.com>
2023-05-26 21:39:43 +0200

    Added CreateTokenView.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+'''
+Views for the User API.
+'''
+from rest_framework import authentication, generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from users.serializers import AuthSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+
+    serializer_class = AuthSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
