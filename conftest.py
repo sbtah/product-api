62a32f61c03afc7f709e20ec64e06910da67ed36
Sbtah <shisastore@protonmail.com>
2023-05-26 21:37:46 +0200

    Added few fixtures to conftest.py.

--- conftest.py
+++ conftest.py
@@ -3,6 +3,46 @@ Fixtures
 '''
 import pytest
 from scraped import models
+from django.contrib.auth import get_user_model
+
+
+@pytest.fixture
+def example_superuser_payload():
+    '''Dictionary of data used for creating example standard user object.'''
+    return {
+        'email': 'admin@example.com',
+        'full_name': 'Admin Admin',
+        'password': 'testpass123!',
+    }
+
+@pytest.fixture
+def example_superuser(example_superuser_payload):
+    '''Pytest fixture for creating example superuser user object.'''
+    data = example_superuser_payload
+    return get_user_model().objects.create_superuser(
+        email=data['email'],
+        full_name=data['full_name'],
+        password=data['password'],
+    )
+
+@pytest.fixture
+def example_user_payload():
+    '''Dictionary of data used for creating example standard user object.'''
+    return {
+        'email': 'user@example.com',
+        'full_name': 'Joe Doe',
+        'password': 'testpass321!',
+    }
+
+@pytest.fixture
+def example_user(example_user_payload):
+    '''Pytest fixture for creating example standard user object.'''
+    data = example_user_payload
+    return get_user_model().objects.create_user(
+        email=data['email'],
+        full_name=data['full_name'],
+        password=data['password'],
+    )
 
 
 @pytest.fixture
@@ -45,3 +85,17 @@ def example_product(example_ecommerce_store):
         name='Test Product',
         url = 'https://test-store/test-product'
     )
+
+# Views related fixtures
+@pytest.fixture
+def api_client():
+    from rest_framework.test import APIClient
+
+    return APIClient()
+
+@pytest.fixture
+def authenticated_client(example_user, api_client):
+    user = example_user
+    api_client.force_authenticate(user=user)
+    yield api_client
+    api_client.force_authenticate(user=None)
