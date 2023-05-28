e2609895d595c0d0edfd36b1f8d332466e8cafc6
Sbtah <shisastore@protonmail.com>
2023-05-28 23:27:55 +0200

    Reorganize urls. Add url for delete view.

--- scraped/urls.py
+++ scraped/urls.py
@@ -6,8 +6,16 @@ app_name = 'scraped'
 
 
 urlpatterns = [
-    path('stores/', views.ecommerce_store_list_view, name="store-list"),
-    path('create/', views.ecommerce_store_create_view, name="store-create"),
+    path(
+        'stores/',
+        views.ecommerce_store_list_view,
+        name="store-list",
+    ),
+    path(
+        'create/',
+        views.ecommerce_store_create_view,
+        name="store-create",
+    ),
     path(
         'stores/<int:pk>/',
         views.ecommerce_store_detail_view,
@@ -17,5 +25,10 @@ urlpatterns = [
         'stores/<int:pk>/update/',
         views.ecommerce_store_update_view,
         name='store-update',
+    ),
+    path(
+        'stores/<int:pk>/delete/',
+        views.ecommerce_store_delete_view,
+        name='store-delete',
     ),
 ]
