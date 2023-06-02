94f90a2df4951efc84c62e527cff499e4fdb7eb0
Sbtah <shisastore@protonmail.com>
2023-06-02 22:53:51 +0200

    Add test cases for detail local store endpoint.

--- tests/test_local_store_api.py
+++ tests/test_local_store_api.py
@@ -11,8 +11,14 @@ from scraped.serializers import (
     LocalStoreSerializer,
 )
 
-LOCAL_STORES_URL = reverse('scraped:local-store-list')
+LOCAL_STORE_LIST_URL = reverse('scraped:local-store-list')
+# LOCAL_STORE_CREATE_URL = reverse('scraped:local-store-create')
 pytestmark = pytest.mark.django_db
+
+
+def detail_url(local_store_id):
+    """Create and return LocalStore detail url."""
+    return reverse('scraped:local-store-detail', args=[local_store_id])
 
 
 class TestPublicLocalStoreApi:
@@ -26,8 +32,33 @@ class TestPublicLocalStoreApi:
         LocalStore list endpoint.
         """
 
-        res = api_client.get(LOCAL_STORES_URL)
+        res = api_client.get(LOCAL_STORE_LIST_URL)
         assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_authentication_required_for_detail_endpoint(
+            self,
+            api_client,
+            example_local_store,
+    ):
+        """
+        Test that authentication is required to access
+        LocalStore detail endpoint.
+        """
+
+        local_store = example_local_store
+        url = detail_url(local_store.id)
+        res = api_client.get(url)
+
+        assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+    # def test_authentication_required_for_create_endpoint(self, api_client):
+    #     """
+    #     Test that authentication is required to access
+    #     LocalStore create endpoint.
+    #     """
+    #
+    #     res = api_client.get(LOCAL_STORE_CREATE_URL)
+    #     assert res.status_code == status.HTTP_401_UNAUTHORIZED
 
 
 class TestPrivateLocalStoreApi:
@@ -37,18 +68,26 @@ class TestPrivateLocalStoreApi:
 
     def test_get_local_stores(
             self,
-            example_ecommerce_store,
-            example_local_store,
+            create_example_local_stores,
             authenticated_client,
-            create_example_local_stores
     ):
         """Test retrieving a list of LocalStores."""
 
         create_example_local_stores
-        res = authenticated_client.get(LOCAL_STORES_URL)
+        res = authenticated_client.get(LOCAL_STORE_LIST_URL)
         local_stores = LocalStore.objects.all().order_by('-id')
         serializer = LocalStoreSerializer(local_stores, many=True)
 
         assert res.status_code == status.HTTP_200_OK
         assert res.data == serializer.data
 
+    def test_get_local_store(self, example_local_store, authenticated_client):
+        """Test get LocalStore detail."""
+
+        local_store = example_local_store
+        url = detail_url(local_store.id)
+        res = authenticated_client.get(url)
+        serializer = LocalStoreDetailSerializer(local_store)
+
+        assert res.status_code == status.HTTP_200_OK
+        assert res.data == serializer.data
